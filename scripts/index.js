5fb52aa02ec76e575d78d72afe5896898b839f04
Denis Atamankin <denisatamankin@yandex.ru>
2024-07-15 15:08:48 +0500

    JScreateCard

--- scripts/index.js
+++ scripts/index.js
@@ -7,3 +7,34 @@
 // @todo: Функция удаления карточки
 
 // @todo: Вывести карточки на страницу
+
+const cardTemplate = document.querySelector('#card-template').content;
+const cardContainer = document.querySelector('.places__list');
+
+function createCard(card, deleteCard) {
+    const cardElement = cardTemplate.querySelector('.card').cloneNode(true);
+
+    cardElement.querySelector('.card__title').textContent = card.name;
+
+    const cardImage = cardElement.querySelector('.card__image');
+
+    cardImage.src = card.link;
+    cardImage.alt = card.name;
+
+    const deleteButton = cardElement.querySelector(".card__delete-button");
+
+    deleteButton.addEventListener('click',function() {deleteCard(deleteElement)});
+    const deleteElement = deleteButton.closest('.card');
+    
+    return cardElement;
+};
+
+function deleteCard(deleteElement) {
+    deleteElement.remove();
+};
+
+initialCards.forEach(function(card) {
+    const cardElement = createCard(card, deleteCard);
+    cardContainer.append(cardElement);
+});
+
